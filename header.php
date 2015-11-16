99b2d42d2ee2bcc56df1166c7d5f4b57706a2803
ong29673 <ppong1993@hotmail.com>
2015-11-16 15:28:29 +0700

    add dashboard template, play with material lite

--- header.php
+++ header.php
@@ -2,6 +2,14 @@
 <html>
 <head>
 	<title> Hospital OPD System </title>
+
+	<link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.6/material.teal-orange.min.css" />
+	<script src="https://storage.googleapis.com/code.getmdl.io/1.0.6/material.min.js"></script>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <head>
 <body>
-	Menu here
+	<!-- Accent-colored raised button with ripple -->
+	<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+  		Button
+	</button>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include_once "header.php";
 ?>
 
-<h1> This is index. </h1>
+<h1>This is index.</h1>
 
 <?php
 include_once "footer.php";
