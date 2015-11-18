f2334a40de0097aa69dd86010d9b9a8ee5b02bf1
shredktp <kornth_ebasicxp@hotmail.com>
2015-11-18 18:34:32 +0700

    Add call script to Add Appointment button

--- viewAppointment_patient.php
+++ viewAppointment_patient.php
@@ -31,53 +31,57 @@ include_once "header.php";
 	</div>
 
 	<script>		
-		//get all appointments then change data below
-		for (var i = 0 ; i < 2; i++) {
-			//prepare data
-			appID = document.createElement("h5");
-			appIDt = document.createTextNode("Appointment ID: getAppID>< ");
-			appID.appendChild(appIDt);
+	function addAppointment() {
+			//get all appointments then change data below
+			// for (var i = 0 ; i < 2; i++) {
+				//prepare data
+				appID = document.createElement("h5");
+				appIDt = document.createTextNode("Appointment ID: getAppID>< ");
+				appID.appendChild(appIDt);
 
-			dep = document.createElement("h5");
-			dept = document.createTextNode("Department: asdfjk");
-			dep.appendChild(dept);
+				dep = document.createElement("h5");
+				dept = document.createTextNode("Department: asdfjk");
+				dep.appendChild(dept);
 
-			dat = document.createElement("h5");
-			datt = document.createTextNode("Date: xx/yy/zzzz");
-			dat.appendChild(datt);
+				dat = document.createElement("h5");
+				datt = document.createTextNode("Date: xx/yy/zzzz");
+				dat.appendChild(datt);
 
-			tim = document.createElement("h5");
-			timt = document.createTextNode("time: xx:xx");
-			tim.appendChild(timt);
+				tim = document.createElement("h5");
+				timt = document.createTextNode("time: xx:xx");
+				tim.appendChild(timt);
 
-			//create button
-			var bt = document.createElement("button");
-			var bttext = document.createTextNode("More Detail");
-			bt.className = "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect";
-			bt.appendChild(bttext);
+				//create button
+				var bt = document.createElement("button");
+				var bttext = document.createTextNode("More Detail");
+				bt.className = "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect";
+				bt.appendChild(bttext);
 
-			//prepare node
-			var nnode = document.createElement("div");
-			var node = document.createElement("div");
-			var t = document.createTextNode(appID);
-			nnode.className = "section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone";
-			node.className = "mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid";
-			
-			//add data to nnode
-			nnode.appendChild(appID);
-			nnode.appendChild(dep);
-			nnode.appendChild(dat);
-			nnode.appendChild(tim);
-			nnode.appendChild(bt);
+				//prepare node
+				var nnode = document.createElement("div");
+				var node = document.createElement("div");
+				var t = document.createTextNode(appID);
+				nnode.className = "section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone";
+				node.className = "mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid";
+				
+				//add data to nnode
+				nnode.appendChild(appID);
+				nnode.appendChild(dep);
+				nnode.appendChild(dat);
+				nnode.appendChild(tim);
+				nnode.appendChild(bt);
 
-			//put everything in box1
-			node.appendChild(nnode);
-			document.getElementById("box1").appendChild(node);
-		};			
+				//put everything in box1
+				node.appendChild(nnode);
+				document.getElementById("box1").appendChild(node);
+			// };
+		}	
 	</script>
 
 	<!-- FAB button with ripple -->
-	<button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect" id="add-button">
+	<button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect" 
+			id="add-button" 
+			onClick="addAppointment()">
 		<i class="material-icons">add</i>
 	</button>
 </main>
