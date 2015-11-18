d667e980a6220e688aaf3e98c69e666ed5a8306c
ong29673 <ppong1993@hotmail.com>
2015-11-18 13:04:41 +0700

    add logon page

--- index.php
+++ index.php
@@ -4,9 +4,16 @@ include_once "header.php";
 
 <main class="mdl-layout__content">
 	<div class="mdl-grid page-content">
-		<div class="mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
-            Main content here
-          </div>
+        <div class="mdl-cell mdl-cell--9-col">
+    		<div class="mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
+            	Main content here
+        	</div>
+  		</div>
+  		<div class="mdl-cell mdl-cell--3-col">
+    		<button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect">
+				<i class="material-icons">add</i>
+			</button>
+  		</div>
 	</div>
 </main>
 
---
+++ patientLogin.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.6/material.teal-orange.min.css" />
+<script src="https://storage.googleapis.com/code.getmdl.io/1.0.6/material.min.js"></script>
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="css/patientLogin.css">
+
+
+<div class="mdl-layout mdl-js-layout mdl-color--grey-100">
+    <main class="mdl-layout__content">
+        <div class="mdl-card mdl-shadow--6dp">
+            <div class="mdl-card__title mdl-color--primary mdl-color-text--white">
+                <h2 class="mdl-card__title-text">Enter HN to Login</h2>
+            </div>
+        <div class="mdl-card__supporting-text">
+                <form action="#">
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" type="text" id="username" />
+                        <label class="mdl-textfield__label" for="username">HN</label>
+                    </div>
+                </form>
+            </div>
+            <div class="mdl-card__actions mdl-card--border">
+                <button class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Log in</button>
+            </div>
+        </div>
+    </main>
+</div>
