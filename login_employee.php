98a1d3da9d61a01c6ac5f3fce8f98337e0bb81a4
shredktp <kornth_ebasicxp@hotmail.com>
2015-11-19 18:29:08 +0700

    change background and logo

--- login_employee.php
+++ login_employee.php
@@ -4,19 +4,22 @@ include_once "header.php";
 
 <link rel="stylesheet" type="text/css" href="css/employeeLogin.css">
 
-<div class="mdl-layout mdl-js-layout mdl-color--grey-100">
+<div class="mdl-layout mdl-js-layout mdl-color--primary" align="center">
     <main class="mdl-layout__content">
-        <div class="mdl-card mdl-shadow--6dp">
-            <div class="mdl-card__title mdl-color--primary mdl-color-text--white">
-                <h2 class="mdl-card__title-text">เข้าสู่ระบบ</h2>
+
+        <img src="HospitalODP_logo.png" style="width:40%; padding-bottom:100px">
+
+        <div class="mdl-card mdl-shadow--6dp" style="margin-bottom:80px">
+            <div class="mdl-card__title mdl-color--white mdl-color-text--primary mdl-card--border">
+                <center><h2 class="mdl-card__title-text">Employee Login</h2></center>
             </div>
             <div class="mdl-card__supporting-text">
                 <form action="#">
-                    <div class="mdl-textfield mdl-js-textfield">
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                         <input class="mdl-textfield__input" type="text" id="username" maxlength="20"/>
                         <label class="mdl-textfield__label" for="username">Username</label>
                     </div>
-                    <div class="mdl-textfield mdl-js-textfield">
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
                         <input class="mdl-textfield__input" type="password" id="password" maxlength="20"/>
                         <label class="mdl-textfield__label" for="password">Password</label>
                     </div>
@@ -31,6 +34,17 @@ include_once "header.php";
     </main>
 </div>
 
-<?php
-include_once "footer.php";
-?>
+<footer>
+    <center><p style="
+    position:fixed;
+    bottom: 0px;
+    width:100%;
+    color:white;">
+
+        Outcube© 2015
+    </p></center>   
+</footer>
+    
+<!-- close body from header.php -->
+</body>
+</html>
