c881dc6280f55ebf5240688b46726d65109228d5
Ong Patinya Petcharit <ppong1993@hotmail.com>
2015-11-22 22:00:22 +0700

    remove comment

--- nav_patient.php
+++ nav_patient.php
@@ -5,14 +5,6 @@
 				<div id="actionbar-left"></div>
 				<div class="mdl-layout-spacer"></div>
 				<div id="actionbar-middle"></div>
-				<!--div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
-        			<label class="mdl-button mdl-js-button mdl-button--icon" for="fixed-header-drawer-exp">
-          				<i class="material-icons">search</i>
-					</label>
-					<div class="mdl-textfield__expandable-holder">
-						<input class="mdl-textfield__input" type="text" name="sample" id="fixed-header-drawer-exp">
-        			</div>
-      			</div-->
       			<div class="mdl-layout-spacer"></div>
       			<div id="actionbar-right"></div>
     		</div>
