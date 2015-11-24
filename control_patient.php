7ca9248652ce71bee5bd34ee14ad15590c52c4a5
sirikorn <kornn.g@live.com>
2015-11-24 18:54:47 +0700

    finish view all appointment and  cancel appointment (:

--- control_patient.php
+++ control_patient.php
@@ -5,12 +5,21 @@
         checkUser($_POST['login_hn']);
     }
 
+    if (isset($_POST['view_appoint'])){
+    	viewAppointment($_POST['view_appoint']);
+	}
+
+	  if (isset($_POST['cancel_appoint'])){
+    	cancelAppointment($_POST['cancel_appoint']);
+	}
+
     function checkUser($hn){
     	$connection = $GLOBALS['connection'];
 		$hn = $connection->real_escape_string($hn);
     	$result = mysqli_query($connection, "SELECT HN,initial,fName,lName FROM patient WHERE HN='$hn'") or die("Query fail: " . mysqli_error($connection));
     	$num_row = mysqli_num_rows($result);
 		if( $num_row == 1 ) {
+			session_start();
 			while ($row = mysqli_fetch_array($result)){
 				$_SESSION['patient_hn'] = $row['HN'];
 				echo $row['HN'];
@@ -22,6 +31,47 @@
 		else {
 			echo 'false';
 		}
+    }
+
+    function viewAppointment($hn){
+  		$a = array();
+  		$b = array();
+  		$connection = $GLOBALS['connection'];
+  		$hno = $connection->real_escape_string($hn);
+  		//$hno = $hn;
+    	$result = mysqli_query($connection, "SELECT * FROM appointment INNER JOIN user ON appointment.doctor_username = user.username 
+    										INNER JOIN department_db ON user.department_id = department_db.department_order
+    										WHERE hn ='$hno'") or die("Query fail: " . mysqli_error($connection));
+  		while ($row = mysqli_fetch_array($result)){
+				$a['appoint_id']= $row['appoint_id'];
+				$a['hn']= $row['HN'];
+				
+				$dateTime = $row['appoint_time'];
+				$datetrim = explode(" ",$dateTime);
+				$date = $datetrim[0];
+				$time = $datetrim[1];
+
+				$a['appoint_time']= $time;
+				$a['appoint_date']= $date;
+				
+				$a['initial']= $row['initial'];
+				$a['fName']= $row['fName'];
+				$a['lName']= $row['lName'];
+				$a['department_id'] = $row['department_id'];
+				$a['department_name'] = $row['department_name'];
+				
+				array_push($b, $a);
+	      }
+	    //array_push($a,$hno);
+    	echo json_encode($b,JSON_FORCE_OBJECT); 
+    } 
+
+      function cancelAppointment($appoint_id){
+        $connection = $GLOBALS['connection'];
+  		$appoint_id = $connection->real_escape_string($appoint_id);
+  		//$hno = $hn;
+    	$result = mysqli_query($connection, "DELETE FROM appointment WHERE appoint_id = '$appoint_id'") or die("Query fail: " . mysqli_error($connection));
+
     }
 
 
