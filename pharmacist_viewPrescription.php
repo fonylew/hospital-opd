9990e756b8ecf6c31fab93f8613a9ed2a0a379c1
Pikachu37 <nabiki.unun@gmail.com>
2015-11-24 16:47:47 +0700

    button is function now yayyyyyyy!!!! but picture is gone instead FU

--- pharmacist_viewPrescription.php
+++ pharmacist_viewPrescription.php
@@ -22,8 +22,8 @@ include_once "nav_pharmacist.php";
 
 <main class="mdl-layout__content">
 	<div class="mdl-grid page-content" id = "box1">
-		<div id = "log0" class="mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--9-col mdl-grid">
-            <div class="section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone mdl-grid">
+		<div id = "log0" class="mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--9-col">
+            <div class="section__text mdl-grid">
             	<div class = "mdl-cell mdl-cell--3-col">
 					<img src="dashboard/images/user.jpg" width="80%" height="80%" border="0" alt=""
 					style="padding:10px; margin-right: auto; margin-left: auto;">			
@@ -45,10 +45,11 @@ include_once "nav_pharmacist.php";
 				<button id = "edi0" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick = "popEdit(this.id)">
 					Edit Prescription
 				</button>
+				<!-- just for testing delete this button later -->
 				<button id = "test" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="printAllprescription()">
 					test
 				</button>
-				<button id = "det0" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="printMedList(this.id)">
+				<button id = "det0" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" onClick="printMedList(0)">
 					show detail
 				</button>
 
@@ -59,118 +60,10 @@ include_once "nav_pharmacist.php";
 
 
 	<script>
-	function printAllprescription(){
-		for (var i = 1; i >= 0; i--) {
-			//document.getElementById("log0").innerHTML="";
-			div1 = document.createElement("div");
-			div1.id = "log"+i;
-			div1.className = "mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--9-col mdl-grid";
-			div2 = document.createElement("div");
-			div2.className = "section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone";
-			div3 = document.createElement("div");
-			div3.className = "mdl-grid";
-
-			div31 = document.createElement("div");
-			div31.className = "mdl-cell mdl-cell--3-col";
-			propic = document.createElement("img");
-			propic.src = "dashboard/images/user.jpg"
-			propic.style = "width:80%; height:80%;padding:10px;margin-right:auto;margin-left:auto;";
-			div31.appendChild(propic);
-
-			div32 = document.createElement("div");
-			div32.className = "mdl-cell mdl-cell--9-col";
-			n = "Hatsune Muki";
-			d = "this is DName";
-			t = "dd/mm/yyyy hh:mm";
-			number = "xxx-xxxx-xx";
-			hName = document.createElement("h5");
-			name1 = document.createTextNode("Name: "+n+i);
-			hName.appendChild(name1);
-			hDoctor = document.createElement("h5");
-			doctor = document.createTextNode("Doctor: "+d);
-			hDoctor.appendChild(doctor);
-			hHN = document.createElement("h5");
-			hN = document.createTextNode("HN: "+number);
-			hHN.appendChild(hN);
-			hTime = document.createElement("h5");
-			time = document.createTextNode("Time: " + t);
-			hTime.appendChild(time);
-			hMedList = document.createElement("h5");
-			medListText = document.createTextNode("Medicine List");
-			hMedList.appendChild(medListText);
-			div32.appendChild(hName);
-			div32.appendChild(hDoctor);
-			div32.appendChild(hHN);
-			div32.appendChild(hTime);
-			div32.appendChild(hMedList);
-
-			div321 = document.createElement("div");
-			div321.id = "medList"+i;
-			div321.className = "mdl-grid";
-			div32.appendChild(div321);
-
-			btAccept = document.createElement("button");
-			btAccept.id = "acc"+i;
-			btAccept.className = "mdl-button mdl-js-button mdl-button--raised mdl-button--colored mdl-js-ripple-effect";
-			acceptText = document.createTextNode("accept");
-			btAccept.onclick="popAccept2(this.id)";
-			btAccept.appendChild(acceptText);
-
-			btEdit = document.createElement("button");
-			btEdit.id = "edi"+i;
-			btEdit.className ="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect";
-			editText = document.createTextNode("Edit Prescription");
-			btEdit.appendChild(editText);
-			btEdit.onclick="popEdit("+btEdit.id+")";
-			
-			//btAccept.addEventListener("click", popAccept(btAccept.id));
-			btDetail = document.createElement("button");
-			btDetail.id = "det"+i;
-			btDetail.className ="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect";
-			detailText = document.createTextNode("Show Detail");
-			btDetail.appendChild(detailText);
-			btDetail.onclick="printMedList(this.id)";
-			
-
-			div3.appendChild(div31);
-			div3.appendChild(div32);
-			div3.appendChild(btAccept);
-			div3.appendChild(btEdit);
-			div3.appendChild(btDetail);
-
-			div2.appendChild(div3);
-			div1.appendChild(div2);
-			document.getElementById("box1").appendChild(div1);
-		};
-	};
-	function printMedList(nid){
-		id = nid.substring(3);
-		document.getElementById("medList"+id).innerHTML = "";
-		for (var i = 2; i >= 0; i--) {
-			nameNode = document.createElement("div");
-			nameNode.className = "mdl-cell mdl-cell--2-col";
-			detailNode = document.createElement("div");
-			detailNode.className = "mdl-cell mdl-cell--10-col";
-
-			medF = document.createElement("h6");
-			medName = document.createTextNode("Med"+i);
-			medF.appendChild(medName);
-			nameNode.appendChild(medF);
-
-			detailF = document.createElement("h6");
-			detaill = document.createTextNode("Let's random the detail!!!! adsfnjklwejmpxohrmfc[i hmc;dfkm[q2mgcj sbfdlmuchupowufpu");
-			detailF.appendChild(detaill);
-			detailNode.appendChild(detailF);
-
-			document.getElementById("medList"+id).appendChild(nameNode);
-			document.getElementById("medList"+id).appendChild(detailNode);
-				
-		};
-	};
 	function deletePrescription(idIn){
-		document.getElementById("log"+idIn).innerHTML = "";
-		document.getElementById("log"+idIn).className = "";
+		document.getElementById("log"+idIn).remove();
 	};
+
 	function popAccept(idIn){
   		showDialog({
 	    	title: '<span id="span_confirm">Confirmation</span>',
@@ -194,6 +87,7 @@ include_once "nav_pharmacist.php";
 		    cancelable: false,		  	
 		})
 	};
+
 	function popEdit(idIn){
 		showDialog({
 		    	title: '<span id="span_confirm">Edit Prescription</span>',
@@ -217,6 +111,128 @@ include_once "nav_pharmacist.php";
 			    },
 			    cancelable: false,		  	
 			})
+	};
+	function printMedList(k){
+		document.getElementById("medList"+k).innerHTML = "";
+		for (var i = 1; i >= 0; i--) {
+			nameNode = document.createElement("div");
+			nameNode.className = "mdl-cell mdl-cell--2-col";
+			detailNode = document.createElement("div");
+			detailNode.className = "mdl-cell mdl-cell--10-col";
+
+			medF = document.createElement("h6");
+			medName = document.createTextNode("Med"+i);
+			medF.appendChild(medName);
+			nameNode.appendChild(medF);
+
+			detailF = document.createElement("h6");
+			detaill = document.createTextNode("Let's random the detail!!!! adsfnjklwejmpxohrmfc[i hmc;dfkm[q2mgcj sbfdlmuchupowufpu");
+			detailF.appendChild(detaill);
+			detailNode.appendChild(detailF);
+
+			document.getElementById("medList"+k).appendChild(nameNode);
+			document.getElementById("medList"+k).appendChild(detailNode);
+				
+		};
+	};
+
+	var k = 0;
+	function printAllprescription(){
+		k = k+1;
+		var div_log0 = document.createElement('div');
+		   div_log0.id = "log0";
+		   div_log0.className = "mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--9-col";
+
+		   var div_0 = document.createElement('div');
+		      div_0.className = "section__text mdl-grid";
+
+		      var div_1 = document.createElement('div');
+		         div_1.className = "mdl-cell mdl-cell--3-col";
+
+		         var img_0 = document.createElement('img');
+		            img_0.src = "dashboard/images/user.jpg";
+		            img_0.height = "80%";
+		            img_0.width = "80%";
+		            img_0.style.padding = "10px";
+		            img_0.style.marginRight = "auto";
+		            img_0.style.marginLeft = "auto";
+		            img_0.border = 0;
+		         div_1.appendChild( img_0 );
+
+		      div_0.appendChild( div_1 );
+
+
+		      var div_2 = document.createElement('div');
+		         div_2.className = "mdl-cell mdl-cell--9-col";
+
+		         var h5_0 = document.createElement('h5');
+		            h5_0.appendChild( document.createTextNode("Name: ") );
+		         div_2.appendChild( h5_0 );
+
+
+		         var h5_1 = document.createElement('h5');
+		            h5_1.appendChild( document.createTextNode("Doctor: ") );
+		         div_2.appendChild( h5_1 );
+
+
+		         var h5_2 = document.createElement('h5');
+		            h5_2.appendChild( document.createTextNode("Time: ") );
+		         div_2.appendChild( h5_2 );
+
+
+		         var h5_3 = document.createElement('h5');
+		            h5_3.appendChild( document.createTextNode("HN: ") );
+		         div_2.appendChild( h5_3 );
+
+
+		         var h5_4 = document.createElement('h5');
+		            h5_4.appendChild( document.createTextNode("Medicine list") );
+		         div_2.appendChild( h5_4 );
+
+
+		         var div_medList0 = document.createElement('div');
+		            div_medList0.className = "mdl-grid";
+		            div_medList0.id = "medList"+k;
+		         div_2.appendChild( div_medList0 );
+
+		      div_0.appendChild( div_2 );
+
+		      /* Raised button with ripple */
+
+		      var button_acc0 = document.createElement('button');
+		         button_acc0.id = "acc"+k;
+		         button_acc0.onclick = function(){
+		            popAccept(this.id)
+		         };
+		         button_acc0.className = "mdl-button mdl-js-button mdl-button--raised mdl-button--colored mdl-js-ripple-effect";
+		         button_acc0.appendChild( document.createTextNode("\n					Accept\n				") );
+		      div_0.appendChild( button_acc0 );
+
+
+		      var button_edi0 = document.createElement('button');
+		         button_edi0.className = "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect";
+		         button_edi0.onclick = function(){
+		            popEdit(this.id)
+		         };
+		         button_edi0.id = "edi"+k;
+		         button_edi0.appendChild( document.createTextNode("\n					Edit Prescription\n				") );
+		      div_0.appendChild( button_edi0 );
+
+		     // printMedList(this.id)
+		      var button_det0 = document.createElement('button');
+		         button_det0.onclick = function(){
+		            printMedList(k)
+		         };
+		         button_det0.className = "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect";
+		         button_det0.id = "det"+k;
+		         button_det0.appendChild( document.createTextNode("\n					show detail\n				") );
+		      div_0.appendChild( button_det0 );
+
+		   div_log0.appendChild( div_0 );
+
+		box1.appendChild( div_log0 );
+
+
 	};
 	</script>
 
