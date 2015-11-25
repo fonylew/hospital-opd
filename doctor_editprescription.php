293b7d79f491e72be18213432918c483b8b58f4e
Ong Patinya Petcharit <ppong1993@hotmail.com>
2015-11-25 22:02:34 +0700

    เพิ่มหน้าสั่งยาใหม่

---
+++ doctor_editprescription.php
@@ -0,0 +1,307 @@
+<?php
+
+include_once "header.php";
+include_once "nav_doctor.php";
+include_once "control_doctor.php";
+
+$medicine_list = getMedicineList();
+$medicine_count = sizeof($medicine_list);
+
+if (isset($_GET['diagnose_appoint_id'])) {
+	$diagnose_appoint_id = $_GET['diagnose_appoint_id'];
+	$diagnose_appoint_hn = $_GET['diagnose_appoint_hn'];
+	$diagnose_appoint_name = $_GET['diagnose_appoint_name'];
+	$diagnose_appoint_date = $_GET['diagnose_appoint_date'];
+	$diagnose_appoint_time = $_GET['diagnose_appoint_time'];
+	$generaldetail = getMedrecGeneralDetail($diagnose_appoint_id);
+} else {
+	$diagnose_appoint_id = '555';
+	$diagnose_appoint_hn = '555';
+	$diagnose_appoint_name = '555';
+	$diagnose_appoint_date = '555';
+	$diagnose_appoint_time = '555';
+	$generaldetail = getMedrecGeneralDetail('7');
+}
+
+?>
+
+<!-- setup actionbar -->
+<script type="text/javascript">
+	$("#actionbar-left").append("<label onClick=\"browserBack()\" class=\"mdl-button mdl-js-button mdl-button--icon\" for=\"fixed-header-drawer-exp\"><i class=\"material-icons\">arrow_back</i></label>");
+	$("#actionbar-middle").append("<div style=\"font-size:x-large\">แก้ไขใบสั่งยา</div>");
+</script>
+<script type="text/javascript">
+	function browserBack(){
+		window.history.back();
+	}
+</script>
+
+<!-- for dropdown -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/material-fullpalette.css">
+<link rel="stylesheet" type="text/css" href="css/jquery.dropdown.css">
+<link rel="stylesheet" type="text/css" href="css/ripples.min.css">
+<link rel="stylesheet" type="text/css" href="css/roboto.min.css">
+
+<!-- for calendar -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap-datepicker3.css">
+
+<!-- custom css -->
+<style type="text/css">
+	.btn.disabled {
+		cursor: not-allowed;
+		opacity: 0.1;
+		filter: alpha(opacity=65);
+		-webkit-box-shadow: none;
+		box-shadow: none;
+	}
+	a.btn.disabled {
+		pointer-events: none;
+	}
+	.selected {
+		background-color: #009688;
+		color: white;
+	}
+	.no-padding {
+		padding: 0px;
+	}
+	.divider {
+		height: 1px;
+		background-color: rgba(0,0,0,0.12);
+		margin-top: 24px;
+		margin-bottom: 24px;
+	}
+</style>
+
+<!-- import function -->
+<script src="js/doctor_editprescription_function.js"></script>
+
+<main class="mdl-layout__content">
+	<div class="mdl-grid page-content">
+		<div class="mdl-cell mdl-cell--9-col mdl-color--white mdl-shadow--2dp mdl-grid" style="padding:24px; color: mdl-primary;">
+			
+			<!-- appointment detail -->
+			<span class="mdl-color-text--primary mdl-cell--12-col" style="font-size: x-large; margin-left: 1em;">รายละเอียดการนัด</span>
+			<div class="mdl-grid mdl-cell--9-col" style="margin-left: 16px; padding-bottom: 0px;">
+				<div class="mdl-cell--12-col mdl-grid no-padding">
+					<div class="section__text mdl-cell mdl-cell--4-col">
+						<span style="font-size: medium; ">หมายเลขนัด: </span>
+					</div>
+					<div class="section__text mdl-cell mdl-cell--6-col">
+						<span id="appId" class="mdl-color-text--primary" style="padding-left: 8px; font-size: large;"><?php echo $diagnose_appoint_id;?></span>
+					</div>
+				</div>
+				
+				<div class="mdl-cell--12-col mdl-grid no-padding">
+					<div class="section__text mdl-cell mdl-cell--4-col">
+						<span style="font-size: medium; ">HN: </span>
+					</div>
+					<div class="section__text mdl-cell mdl-cell--6-col">
+						<span id="hn" class="mdl-color-text--primary" style="padding-left: 8px; font-size: medium;"><?php echo $diagnose_appoint_hn;?></span>
+					</div>
+				</div>
+				
+				<div class="mdl-cell--12-col mdl-grid no-padding">
+					<div class="section__text mdl-cell mdl-cell--4-col">
+						<span style="font-size: medium; ">ชื่อผู้ป่วย: </span>
+					</div>
+					<div class="section__text mdl-cell mdl-cell--6-col">
+						<span id="patientName" class="mdl-color-text--primary" style="padding-left: 8px; font-size: large;"><?php echo $diagnose_appoint_name;?></span>
+					</div>
+				</div>
+				
+				<div class="mdl-cell--12-col mdl-grid no-padding">
+					<div class="section__text mdl-cell mdl-cell--4-col">
+						<span style="font-size: medium; ">วัน: </span>
+					</div>
+					<div class="section__text mdl-cell mdl-cell--6-col">
+						<span id="date" class="mdl-color-text--primary" style="padding-left: 8px; font-size: large;"><?php echo $diagnose_appoint_date;?></span>
+					</div>
+				</div>
+				
+				<div class="mdl-cell--12-col mdl-grid no-padding">
+					<div class="section__text mdl-cell mdl-cell--4-col">
+						<span style="font-size: medium; ">เวลา: </span>
+					</div>
+					<div class="section__text mdl-cell mdl-cell--6-col">
+						<span id="time" class="mdl-color-text--primary" style="padding-left: 8px; font-size: large;"><?php echo $diagnose_appoint_time;?></span>
+					</div>
+				</div>
+
+			</div>
+
+			<!-- patient picture -->
+			<div class="mdl-cell--3-col" style="margin: auto;">
+				<img src="dashboard/images/dog.png" style="width: 100%;">
+			</div>
+
+			<div class="mdl-cell--12-col divider"></div>
+
+			<!-- patient info from nurse -->
+			<span class="mdl-color-text--primary mdl-cell--12-col" style="font-size: x-large; margin-left: 1em; margin-bottom: 16px;">การตรวจร่างกายเบื้องต้น</span>
+			<div class="mdl-cell--12-col" style="overflow: auto;">
+				<table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp" style="width: auto; margin-left: auto; margin-right: auto;">
+					<thead>
+						<tr>
+							<th>น้ำหนัก</th>
+							<th>ส่วนสูง</th>
+							<th>อุณหภูมิร่างกาย</th>
+							<th>อัตราการเต้นของหัวใจ</th>
+							<th>ความดันโลหิต</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr id = "generaldetail_table">
+						</tr>
+					</tbody>
+				</table>
+			</div>
+
+			<div class="mdl-cell--12-col divider" style="margin-top:32px;"></div>
+
+			<!-- diagnose memo -->
+			<span class="mdl-color-text--primary mdl-cell--12-col" style="font-size: x-large; margin-left: 1em;">บันทึกการตรวจ</span>
+			<div class="mdl-cell--12-col mdl-grid" style="margin-left: auto; margin-right: auto; padding-top: 0px;">
+				<div class="mdl-cell--5-col" style="margin-top: 14px; margin-left: auto; margin-right: auto;">
+				<!--<div id="dropdown-menu" class="mdl-cell--5-col" style="margin-top: 14px; margin-left: auto; margin-right: auto;">-->
+					<div class="form-group" style="margin-top: 0px;">
+						<select id="s1" class="form-control" onchange="changeIllnessList()" disabled>
+							<option value="-">-- ประเภทรหัสโรค --</option>
+							<option value="ICD10">ICD10</option>
+							<option value="SNOMED">SNOMED</option>
+							<option value="DRG">DRG</option>
+						</select>
+					</div>
+				</div>
+				<div class="mdl-cell--5-col" style="margin-left: auto; margin-right: auto;">
+					<div class="form-group" style="margin-top: 15px;">
+						<select id="s2" class="form-control"  disabled>
+							<option value="-">-- รหัสโรค --</option>
+						</select>
+					</div>
+				</div>
+				<div class="mdl-cell--11-col" style="margin-left: auto; margin-right: auto;">
+					<form action="#">
+						<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" style="width: 100%;">
+							<input class="mdl-textfield__input" type="text" id="description" disabled/>
+							<label class="mdl-textfield__label" for="username" id="user-label">รายละเอียด</label>
+						</div>
+					</form>
+				</div>
+			</div>
+
+			<div class="mdl-cell--12-col divider" style="margin-top:32px;"></div>
+
+			<!-- prescription -->
+			<span class="mdl-color-text--primary mdl-cell--12-col" style="font-size: x-large; margin-left: 1em;">การสั่งยา</span>
+
+			<div id="prescription-box" class="mdl-cell--12-col mdl-grid" style="margin: 0; padding: 0;"></div>
+
+			<div class="mdl-cell--12-col mdl-grid">
+				<div class="mdl-cell--4-col" style="margin-left: auto; margin-right: auto;">
+					<center>
+						<button onclick="addMedicineClicked()" class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab" style="background-color: rgba(220,220,220,1); margin-bottom: 10px; ">
+							<i class="material-icons">add</i>
+						</button>
+						<br>
+						<span>เพิ่มชนิดยา</span>
+					</center>
+				</div>
+			</div>
+			
+			<div class="mdl-cell--12-col divider"></div>
+
+			<!-- next appointment -->
+			<span class="mdl-color-text--primary mdl-cell--12-col" style="font-size: x-large; margin-left: 1em;">การนัดครั้งถัดไป</span>
+			<div class="mdl-cell--3-col" style="margin: auto; padding-top: 16px; padding-left: 32px;">
+				<label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" onchange="onToggleNextApp(this)" for="next-app-checkbox">
+					<input type="checkbox" id="next-app-checkbox" class="mdl-checkbox__input" disabled>
+					<span style="font-size: medium; font-weight: 300;">นัดครั้งถัดไป</span>
+				</label>
+			</div>
+			<div class="mdl-cell--4-col" style="margin-left: auto; margin-right: auto;">
+				<form action="#">
+					<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" style="width: 100%;">
+						<input class="mdl-textfield__input" type="text" id="code" disabled/>
+						<label class="mdl-textfield__label" for="username" id="user-label">จำนวน</label>
+					</div>
+				</form>
+			</div>
+			<div id="dropdown-menu" class="mdl-cell--4-col" style="margin-top: 14px; margin-left: auto; margin-right: auto;">
+				<div class="form-group" style="margin-top: 0px;">
+					<select id="s2" class="form-control" disabled>
+						<option value="-">-- หน่วย --</option>
+						<option value="unit_1">วัน</option>
+						<option value="unit_2">สัปดาห์</option>
+						<option value="unit_2">เดือน</option>
+					</select>
+				</div>
+			</div>
+
+
+			<center class="mdl-cell--12-col" style="margin-top: 16px;">
+				<button class="mdl-button mdl-button--raised mdl-button--colored" style="margin-left: 16px;" onClick="showSubmitDiagnoseConfirm()">
+					บันทึกการแก้ไขใบสั่งยา
+				</button>
+			</center>
+		</div>
+		<div class="mdl-cell mdl-cell--3-col">
+
+		</div>
+	</div>
+</main>
+
+<!-- import custom js -->
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.dropdown.js"></script>
+<script src="js/ripples.min.js"></script>
+
+<script>
+
+	var medicine_list = <?php echo json_encode($medicine_list,JSON_FORCE_OBJECT)?>;
+	var medicine_count = <?php echo json_encode($medicine_count,JSON_FORCE_OBJECT)?>;	
+	var diagnose_appoint_id = <?php echo json_encode($diagnose_appoint_id,JSON_FORCE_OBJECT)?>;
+	var medicineCount = 0;
+
+	var generaldetail = <?php echo json_encode($generaldetail,JSON_FORCE_OBJECT)?>;
+	$("#generaldetail_table").append("<td style='font-size: large;''>"+generaldetail.weight+" kg.</td>");
+	$("#generaldetail_table").append("<td style='font-size: large;''>"+generaldetail.height+" cm.</td>");
+	$("#generaldetail_table").append("<td style='font-size: large;''>"+generaldetail.temperature+" ํC</td>");
+	$("#generaldetail_table").append("<td style='font-size: large;''>"+generaldetail.heartRate+" ครั้ง/นาที</td>");
+	$("#generaldetail_table").append("<td style='font-size: large;''>"+generaldetail.bloodPressure+"</td>");
+
+	$("#dropdown-menu select").dropdown();
+	
+	function addMedicineClicked() {
+		medicineCount++;
+		addPrescription(medicineCount);
+	}
+
+	function changeIllnessList() {
+		var illness_type = document.getElementById("s1").value;
+		if (illness_type != '-') {
+			$.ajax({
+	            url: 'control_doctor.php',
+	            type: 'POST',
+	            data: {illness_type: illness_type},
+	            dataType: "json",
+	            success: function(data) {
+	            	$('#s2').empty();
+	            	$('#s2').append('<option value="-">-- รหัสโรค --</option>');
+	                for (var i = 0; i < Object.keys(data).length; i++) {
+	                	$('#s2').append('<option value="'+data[i].illness_code+'">'+data[i].illness_name+'</option>');
+	                }
+	            }
+        	});
+		} 
+	}
+
+</script>
+
+<script src="js/bootstrap-datepicker.min.js"></script>
+<script src="js/bootstrap-datepicker.th.min.js"></script>
+
+<?php
+include_once "nav_end.php";
+include_once "footer.php";
+?>
---
+++ js/doctor_editprescription_function.js
@@ -0,0 +1,205 @@
+function showClearConfirm(){
+	showDialog({
+		title:
+		'<span style="font-size: x-large; " class="mdl-color-text--primary">ลบบันทึกการตรวจทั้งหมด</span>',
+		text:
+		'ลบบันทึกการตรวจทั้งหมดหรือไม่ ?',
+		negative: {
+			id: 'cancel-button',
+			title: 'ยกเลิก',
+			onClick: function() { 
+
+			}
+		},
+		positive: {
+			id: 'ok-button',
+			title: 'ลบ',
+			onClick: function() {
+				location.reload();
+			}
+		},
+		cancelable: true,
+	});
+}
+
+function showSubmitDiagnoseConfirm(){
+	showDialog({
+		title:
+		'<span style="font-size: x-large; " class="mdl-color-text--primary">บันทึกผลการตรวจ</span>',
+		text:
+		'ต้องการบันทึกผลการตรวจหรือไม่ ?',
+		negative: {
+			id: 'cancel-button',
+			title: 'ยกเลิก',
+			onClick: function() { 
+
+			}
+		},
+		positive: {
+			id: 'ok-button',
+			title: 'บันทึก',
+			onClick: function() {
+				var prescriptions = [];
+				for (var i = 1; i <= medicineCount; i++) {
+					var elementExists = document.getElementById("medName"+i);
+					if(elementExists != null && elementExists.value != '-') {
+						var prescription = [document.getElementById("medName"+i).value,document.getElementById("medAmount"+i).value,document.getElementById("medHowToUse"+i).value];
+						prescriptions[i-1] = prescription;
+					}
+				}	
+				prescriptions = JSON.stringify(prescriptions);
+		        $.ajax({
+		            url: 'control_doctor.php',
+		            type: 'POST',
+		            data: {diagnose_code: document.getElementById("s2").value,diagnose_description: document.getElementById("description").value, diagnose_appointid: diagnose_appoint_id, diagnose_prescriptions: prescriptions},
+		            dataType: "text",
+		            success: function(data) {
+		            	location.href = "doctor_viewappointment.php";
+		            }
+		        });
+			}
+		},
+		cancelable: true,
+	});
+}
+
+function onToggleNextApp(){
+	console.log("toggle");
+	if($('#next-app-checkbox').checked){
+		console.log("check");
+
+	} else {
+		console.log("uncheck");
+
+	}
+}
+
+function addPrescription(medicineCount){
+	var div_0 = document.createElement('div');
+	div_0.style.marginTop = "32px";
+	div_0.style.marginBottom = "16px";
+	div_0.style.marginLeft = "auto";
+	div_0.style.marginRight = "auto";
+	div_0.style.position = "relative";
+	div_0.className = "mdl-cell--8-col mdl-shadow--2dp mdl-grid";
+
+	var div_1 = document.createElement('div');
+	div_1.style.marginLeft = "auto";
+	div_1.style.marginRight = "auto";
+	div_1.className = "mdl-cell--6-col";
+
+	var div_2 = document.createElement('div');
+	div_2.className = "form-group";
+	div_2.style.width = "100%";
+	div_2.style.marginTop ="15px";
+
+	var input_medName = document.createElement('select');
+	input_medName.id = "medName"+medicineCount;
+	input_medName.className = "form-control";
+
+	var option = document.createElement("option");
+	option.text = "-- ชื่อยา --";
+	option.value = "-";
+	input_medName.add(option);
+
+	for (var i = 0; i < medicine_count; i++) {
+		var option = document.createElement("option");
+		option.text = medicine_list[i].med_name;
+		option.value = medicine_list[i].med_code;
+		input_medName.add(option);
+	}
+
+	div_2.appendChild( input_medName );
+
+	div_1.appendChild( div_2 );
+
+	div_0.appendChild( div_1 );
+
+	var div_3 = document.createElement('div');
+	div_3.className = "mdl-cell--6-col";
+	div_3.style.marginLeft = "auto";
+	div_3.style.marginRight = "auto";
+
+	var form_1 = document.createElement('form');
+	form_1.action = "#";
+
+	var div_4 = document.createElement('div');
+	div_4.className = "mdl-textfield mdl-js-textfield mdl-textfield--floating-label";
+	div_4.style.width = "100%";
+
+	var input_medAmount = document.createElement('input');
+	input_medAmount.className = "mdl-textfield__input";
+	input_medAmount.type = "text";
+	input_medAmount.id = "medAmount"+medicineCount;
+	div_4.appendChild( input_medAmount );
+
+
+	var label_userLabel_2 = document.createElement('label');
+	label_userLabel_2.className = "mdl-textfield__label";
+	label_userLabel_2.htmlFor = "username";
+	label_userLabel_2.id = "user-label";
+	label_userLabel_2.appendChild( document.createTextNode("จำนวน (เม็ด)") );
+	div_4.appendChild( label_userLabel_2 );
+
+	form_1.appendChild( div_4 );
+
+	div_3.appendChild( form_1 );
+
+	div_0.appendChild( div_3 );
+
+
+	var div_5 = document.createElement('div');
+	div_5.style.marginLeft = "auto";
+	div_5.style.marginRight = "auto";
+	div_5.className = "mdl-cell--12-col";
+
+	var form_2 = document.createElement('form');
+	form_2.action = "#";
+
+	var div_6 = document.createElement('div');
+	div_6.className = "mdl-textfield mdl-js-textfield mdl-textfield--floating-label";
+	div_6.style.width = "100%";
+
+	var input_medHowToUse = document.createElement('input');
+	input_medHowToUse.type = "text";
+	input_medHowToUse.id = "medHowToUse"+medicineCount;
+	input_medHowToUse.className = "mdl-textfield__input";
+	div_6.appendChild( input_medHowToUse );
+
+
+	var label_userLabel_3 = document.createElement('label');
+	label_userLabel_3.id = "user-label";
+	label_userLabel_3.htmlFor = "username";
+	label_userLabel_3.className = "mdl-textfield__label";
+	label_userLabel_3.appendChild( document.createTextNode("วิธีใช้ยา") );
+	div_6.appendChild( label_userLabel_3 );
+
+	form_2.appendChild( div_6 );
+
+	div_5.appendChild( form_2 );
+
+	div_0.appendChild( div_5 );
+
+
+	var button_0 = document.createElement('button');
+	button_0.style.position = "absolute";
+	button_0.style.top = "-20px";
+	button_0.style.right = "-20px";
+	button_0.style.backgroundColor = "rgba(220,220,220,1)";
+	button_0.className = "mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab remove-prescription";
+
+	var i_0 = document.createElement('i');
+	i_0.className = "material-icons";
+	i_0.appendChild( document.createTextNode("clear") );
+	button_0.appendChild( i_0 );
+
+	div_0.appendChild( button_0 );
+
+	document.getElementById("prescription-box").appendChild( div_0 );
+
+	$('button.remove-prescription').click(function(){
+		$(this).parent().remove();
+	});
+
+	componentHandler.upgradeAllRegistered();
+}
